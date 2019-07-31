4d5cbc494340410e0b1f8af562bcd80fed6db3a8
denio-rus <denio@yandex.ru>
2019-07-31 14:34:35 +0700

    Add index for sites(admin-panel)

--- app/controllers/sites_controller.rb
+++ app/controllers/sites_controller.rb
@@ -1,7 +1,19 @@
 class SitesController < ApplicationController
+  before_action :authenticate_user!, only: [:index, :destroy]
+
   def analysis
     @site = Services::SiteAnalyzer.new(params[:domain]).call
   end
 
   def request_form; end
+
+  def index
+    @sites = Site.all
+  end
+
+  def destroy
+    @site = Site.find(params[:id])
+    @site.destroy
+    redirect_to sites_path, alert: "Site was successfuly deleted from database."
+  end
 end
