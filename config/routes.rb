b3c94e1536fad99a174800f7ae53347c72d647bb
denio-rus <denio@yandex.ru>
2019-07-31 13:56:04 +0700

    Add devise, user

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
+  devise_for :users
   root to: 'sites#request_form'
 
-  get 'sites/request_form'
-  get 'sites/analysis'
-
-  resources :pages, only: :show
+  resources :sites, only: [:index, :destroy] do
+    collection do
+      get :request_form
+      get :analysis
+    end
+    
+    resources :pages, only: :show, shallow: true
+  end
 end
