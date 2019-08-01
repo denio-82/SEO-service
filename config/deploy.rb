86371fcdf1b016e397b105db1c30842fbccad33c
denio-rus <denio@yandex.ru>
2019-08-01 17:40:23 +0700

    Deploy with Unicorn

---
+++ config/deploy.rb
@@ -0,0 +1,18 @@
+# config valid for current version and patch releases of Capistrano
+lock "~> 3.11.0"
+
+set :application, "SEO-service"
+set :repo_url, "git@github.com:denio-rus/SEO-service.git"
+
+# Default deploy_to directory is /var/www/my_app_name
+set :deploy_to, "/home/deployer/SEO-service"
+set :deploy_user, 'deployer'
+
+# Default value for :linked_files is []
+append :linked_files, "config/database.yml", "config/master.key"
+
+# Default value for linked_dirs is []
+append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
+
+after 'deploy:publishing', 'unicorn:restart'
+
